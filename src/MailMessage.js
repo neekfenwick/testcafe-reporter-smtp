8cc7feafb31bcd37cad9ad366874740ef99115a8
Nick Fenwick <neek@nickfenwick.com>
2026-03-20 23:27:59 +0700

    Widespread updates by Claude.

--- src/MailMessage.js
+++ src/MailMessage.js
@@ -2,122 +2,67 @@ const nodemailer = require('nodemailer');
 const handlebars = require('handlebars');
 const fs = require('fs');
 
-function MailMessage (info, config) {
-    this.info = info;
-    this.config = config;
+/**
+ * Email message class for sending TestCafe results via SMTP
+ */
+class MailMessage {
+    /**
+     * @param {Object} info - Email content information for template data
+     * @param {Object} config - Configuration including SMTP options and template paths
+     */
+    constructor(info, config) {
+        this.info = info;
+        this.config = config;
+    }
+
+    /**
+     * Send the email with test results
+     * @returns {Promise<string>} The text body of the email
+     */
+    async send() {
+        try {
+            // Compile templates from handlebars source
+            const htmlTemplate = fs.readFileSync(this.config.htmlTemplateFile, 'utf-8');
+            const htmlTmpl = handlebars.compile(htmlTemplate);
+            const textTemplate = fs.readFileSync(this.config.textTemplateFile, 'utf-8');
+            const textTmpl = handlebars.compile(textTemplate);
+
+            // Generate email body from template
+            const htmlBody = htmlTmpl(this.info);
+            const textBody = textTmpl(this.info);
+
+            // Configure email transport
+            if (this.config.bDebug) {
+                console.log('Transport options:', this.config.smtpOptions);
+            }
+            
+            const transporter = nodemailer.createTransporter(this.config.smtpOptions);
+
+            // Setup email options
+            const mailOptions = {
+                from: this.info.from,
+                to: this.info.recipients,
+                subject: this.info.subject,
+                text: textBody,
+                html: htmlBody
+            };
+
+            // Handle test mocking
+            if (this.config.smtpOptions.host === 'unit-test-mock') {
+                console.log('Unit Test Mock Host defined - do not send emails but consider run successful.');
+                console.log('Text email body was:\n********************\n' + textBody + '\n******************');
+            } else {
+                // Send email
+                const info = await transporter.sendMail(mailOptions);
+                console.log('Message sent: %s', info.messageId);
+            }
+
+            return textBody;
+        } catch (error) {
+            console.error('Failed to send email:', error);
+            throw error;
+        }
+    }
 }
 
-MailMessage.prototype.send = function send () {
-    // First compile the template from handlebars source.
-    const htmlTemplate = fs.readFileSync(this.config.htmlTemplateFile, 'utf-8');
-    const htmlTmpl = handlebars.compile(htmlTemplate);
-    const textTemplate = fs.readFileSync(this.config.textTemplateFile, 'utf-8');
-    const textTmpl = handlebars.compile(textTemplate);
-
-    // Run our data object through the template to produce the email body.
-    const htmlBody = htmlTmpl(this.info);
-    const textBody = textTmpl(this.info);
-    // console.log('Produced output: ' + htmlBody);
-
-    // Prepare email transport
-    if (this.config.bDebug) console.log('Transport options: ', this.config.smtpOptions);
-    const transporter = nodemailer.createTransport(this.config.smtpOptions);
-
-    // setup email data with unicode symbols
-    const mailOptions = {
-        from:    this.info.from, // sender address
-        to:      this.info.recipients,
-        subject: this.info.subject, // Subject line
-        text:    textBody, // plain text body
-        html:    htmlBody // html body
-    };
-
-    // console.log('mailOptions: ', mailOptions);
-    // console.log('smtpOptions: ', this.config.smtpOptions);
-
-    if (this.config.smtpOptions.host === 'unit-test-mock') {
-        // Gulpfile.js has indicated we are running a unit test, don't try to actually send an email.
-        console.log('Unit Test Mock Host defined - do not send emails but consider run successful.');
-        console.log('Text email body was:\n********************\n' + textBody + '\n******************');
-    }
-    else {
-        // send mail with defined transport object
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.log(error);
-                return;
-            }
-            console.log('Message sent: %s', info.messageId);
-
-            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-        });
-    }
-
-    return textBody;
-};
-
 module.exports = MailMessage;
-
-// module.exports = function () {
-//     return {
-//         /**
-//          * info - object containing email contents information, to be passed into the Handlebars template.
-//          * config - configuration e.g. smtpOptions.
-//          */
-//         constructor (info, config) {
-//             console.log(`MailMessage ctor here, config ${config}`);
-//             this.info = info;
-//             this.config = config;
-//         },
-
-//         send () {
-//             // First compile the template from handlebars source.
-//             const htmlTemplate = fs.readFileSync(this.config.htmlTemplateFile, 'utf-8');
-//             const htmlTmpl = handlebars.compile(htmlTemplate);
-//             const textTemplate = fs.readFileSync(this.config.textTemplateFile, 'utf-8');
-//             const textTmpl = handlebars.compile(textTemplate);
-
-//             // Run our data object through the template to produce the email body.
-//             const htmlBody = htmlTmpl(this.info);
-//             const textBody = textTmpl(this.info);
-//             // console.log('Produced output: ' + htmlBody);
-
-//             // Prepare email transport
-//             // console.log('Transport options: ', this.config.smtpOptions);
-//             const transporter = nodemailer.createTransport(this.config.smtpOptions);
-
-//             // setup email data with unicode symbols
-//             // console.log('User info: ', userInfo);
-//             const mailOptions = {
-//                 from:    this.info.from, // sender address
-//                 to:      this.info.recipients,
-//                 subject: this.info.subject, // Subject line
-//                 text:    textBody, // plain text body
-//                 html:    htmlBody // html body
-//             };
-
-//             // console.log('mailOptions: ', mailOptions);
-//             // console.log('smtpOptions: ', this.config.smtpOptions);
-
-//             if (this.config.smtpOptions.host === 'unit-test-mock') {
-//                 // Gulpfile.js has indicated we are running a unit test, don't try to actually send an email.
-//                 console.log('Unit Test Mock Host defined - do not send emails but consider run successful.');
-//                 console.log('Text email body was:\n********************\n' + textBody + '\n******************');
-//             }
-//             else {
-//                 // send mail with defined transport object
-//                 transporter.sendMail(mailOptions, (error, info) => {
-//                     if (error) {
-//                         console.log(error);
-//                         return;
-//                     }
-//                     console.log('Message sent: %s', info.messageId);
-
-//                     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-//                     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-//                 });
-//             }
-//         }
-//     };
-// };
--- src/index.js
+++ src/index.js
@@ -8,19 +8,42 @@
  *   TESTCAFE_SMTP_TO_LIST=me@example.com,mum@example.com
  * Email construction and sending is performed in MailMessage, and is separated
  * from the config mechanism (all values are passed in as objects).
- * @type Module envs|Module envs
  */
 const MailMessage = require('./MailMessage');
 const dotenv = require('dotenv');
+const { join } = require('path');
+
 const configSource = process.env.TESTCAFE_SMTP_CONFIG_FILE;
 const bDebug = process.env.TESTCAFE_SMTP_DEBUG;
 const dotenvOptions = configSource ? { path: configSource } : null;
-const path = require('path');
 
 dotenv.config(dotenvOptions);
 
-function getEnv (key, _default) {
-    return process.env.hasOwnProperty(key) ? process.env[key] : _default;
+/**
+ * Get environment variable with fallback default
+ * @param {string} key - Environment variable name
+ * @param {string} _default - Default value if not set
+ * @returns {string} Environment variable value or default
+ */
+function getEnv(key, _default) {
+    return Object.prototype.hasOwnProperty.call(process.env, key) ? process.env[key] : _default;
+}
+
+/**
+ * Validate required SMTP configuration
+ * @param {Object} smtpOptions - SMTP configuration object
+ * @throws {Error} If required configuration is missing
+ */
+function validateSmtpConfig(smtpOptions) {
+    if (!smtpOptions.host || smtpOptions.host === 'smtp.example.com') {
+        throw new Error('TESTCAFE_SMTP_SMTPHOST must be configured with a valid SMTP host');
+    }
+    if (!smtpOptions.auth?.user || smtpOptions.auth.user === 'username') {
+        throw new Error('TESTCAFE_SMTP_SMTPUSER must be configured with a valid username');
+    }
+    if (!smtpOptions.auth?.pass || smtpOptions.auth.pass === 'password') {
+        throw new Error('TESTCAFE_SMTP_SMTPPASS must be configured with a valid password');
+    }
 }
 
 module.exports = function () {
@@ -30,13 +53,15 @@ module.exports = function () {
         reportTaskStart (startTime, userAgents, testCount) {
             // Include secure if it is defined, else let it default.
             if (bDebug) console.log(`Loading config from ${configSource}... dotenv options ${JSON.stringify(dotenvOptions)}`);
-            var secure = getEnv('TESTCAFE_SMTP_SECURE', '');
+            
+            const secure = getEnv('TESTCAFE_SMTP_SECURE', '');
 
             this.reportOnlyFailures = getEnv('TESTCAFE_SMTP_REPORTONLYFAILURES', 'true');
             this.startTime = startTime;
             this.testCount = testCount;
             this.fixtureInfo = {}; // keyed by fixture name, data for email template
             this.messages = [];
+            
             this.smtpOptions = {
                 host: getEnv('TESTCAFE_SMTP_SMTPHOST', 'smtp.example.com'),
                 port: getEnv('TESTCAFE_SMTP_SMTPPORT', 587),
@@ -46,8 +71,22 @@ module.exports = function () {
                 },
                 logger: getEnv('TESTCAFE_SMTP_LOGGER', 'false') === 'true'
             };
-            if (secure)
+            
+            if (secure) {
                 this.smtpOptions.secure = secure;
+            }
+            
+            // Only validate config in non-test environments
+            if (this.smtpOptions.host !== 'unit-test-mock') {
+                try {
+                    validateSmtpConfig(this.smtpOptions);
+                } catch (error) {
+                    console.error('SMTP Configuration Error:', error.message);
+                    console.error('Please check your environment variables or config file.');
+                    throw error;
+                }
+            }
+            
             if (bDebug) console.log(`Built smtpOptions: ${JSON.stringify(this.smtpOptions)}`);
 
             this.messages.push(`Starting testcafe ${startTime}.\n Running tests in: ${userAgents}`);
@@ -94,7 +133,6 @@ module.exports = function () {
 
             if (!from) {
                 const userInfo = require('os').userInfo();
-
                 from = `"${userInfo.username}" <${userInfo.email}>`;
             }
 
@@ -111,13 +149,19 @@ module.exports = function () {
             }, {
                 bDebug:           bDebug,
                 smtpOptions:      this.smtpOptions,
-                htmlTemplateFile: getEnv('TESTCAFE_SMTP_HTML_EMAIL_TEMPLATE', path.join(__dirname, 'templates/defaultHTML.handlebars')),
-                textTemplateFile: getEnv('TESTCAFE_SMTP_TEXT_EMAIL_TEMPLATE', path.join(__dirname, 'templates/defaultTEXT.handlebars'))
+                htmlTemplateFile: getEnv('TESTCAFE_SMTP_HTML_EMAIL_TEMPLATE', join(__dirname, 'templates/defaultHTML.handlebars')),
+                textTemplateFile: getEnv('TESTCAFE_SMTP_TEXT_EMAIL_TEMPLATE', join(__dirname, 'templates/defaultTEXT.handlebars'))
             });
 
-            const textBody = mail.send();
-
-            this.write(textBody);
+            // Handle async email sending
+            mail.send()
+                .then((textBody) => {
+                    this.write(textBody);
+                })
+                .catch((error) => {
+                    console.error('Failed to send email report:', error.message);
+                    this.write('Error sending email report. Check console for details.');
+                });
         }
     };
 };
